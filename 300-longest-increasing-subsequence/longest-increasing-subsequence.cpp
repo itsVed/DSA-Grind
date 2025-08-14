ce5813522c5a43f200460f562e63f78d4c72e53a
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2025-08-14 20:09:21 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (88.10%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int n;
-    vector<vector<int>> dp;
-
-    int f(int i, int prev, vector<int> &nums){
-        
-        if(i >= n) return 0;
-
-        if(dp[i][prev + 1] != -1) return dp[i][prev + 1];
-
-        int take = 0;
-        if(prev == -1 || nums[prev] < nums[i]){
-            take = 1 + f(i + 1, i, nums);
-        }
-
-        int skip = f(i + 1, prev, nums);
-
-        return dp[i][prev + 1] = max(take, skip);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
         n = nums.size();
-        dp.assign(n, vector<int>(n + 1, -1));
+        vector<int> ans;
+        ans.push_back(nums[0]);
 
-        return f(0, -1, nums);
+        for(int i=1; i < n;i++){
+            auto j = lower_bound(ans.begin(), ans.end(), nums[i]);
+
+            if(j == ans.end()) ans.push_back(nums[i]);
+            else *j = nums[i];
+        }
+
+        return ans.size();
     }
 };
