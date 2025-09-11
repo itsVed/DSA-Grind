86458905610272cabd12210a8a6e2bcc0f43d0e0
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2025-09-11 15:12:03 +0530

    Time: 45 ms (24.57%) | Memory: 72 MB (71.86%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -9,14 +9,14 @@ var threeSum = function(nums) {
     let n = nums.length;
 
     for(let i=0; i < n - 2; i++){
-        if(i > 0 && nums[i] == nums[i - 1]) continue;
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
 
         let l = i + 1, r = n - 1;
 
         while(l < r){
             let sum = nums[i] + nums[l] + nums[r];
 
-            if(sum == 0){
+            if(sum === 0){
                 a.push([nums[i], nums[l], nums[r]]);
                 l++;
                 r--;
