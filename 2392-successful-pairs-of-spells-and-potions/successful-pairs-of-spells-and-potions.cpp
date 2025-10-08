5b49fbc7b1fa4e7b83e3e67facdc7ee6b3e54664
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2025-10-08 09:25:54 +0530

    Time: 44 ms (53.89%) | Memory: 125 MB (94.62%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
+
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        vector<int> ans;
-        int n = spells.size();
-        int m = potions.size();
+        int n = potions.size();
+        vector<int> ans(spells.size());
+        sort(potions.begin(), potions.end());
 
-        sort(potions.begin(), potions.end()); // sorting the potions array
-        int left, right;
+        for(int i=0; i < spells.size(); i++){
 
-        for(int i=0; i < n; i++){
-            left = 0;
-            right = m - 1;
-            int pair = 0;
+            int l = 0, r = n - 1;
+            int len = n;
 
-            while(left <= right){ // using the binary search so that i can find the index and subtract it from n and i got the no. of pairs
-                int mid = (right - left)/2 + left;
-                long long mul = (long long) potions[mid] * spells[i]; // calculating the product
+            while(l <= r){
+                int mid = l + (r - l)/2;
+                long long product = 1LL * potions[mid] * spells[i];
 
-                if(mul >= success){ // if i get the true then i search for the left side so i got max ans
-                    pair = m - mid;
-                    right = mid - 1;
+                if(product >= success){
+                    len = mid;
+                    r = mid - 1;
                 }
                 else{
-                    left = mid + 1; // searching the right side
+                    l = mid + 1;
                 }
-            }   
+            }
 
-            ans.push_back(pair); // storig the no. of pairs
+            ans[i] = (n - len);
+
         }
 
         return ans;
-    
     }
 };
