5bcdda0ebcfa3be242f85c5d9d8b7a9e400da068
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2025-10-22 15:00:06 +0530

    Time: 108 ms (55.95%) | Memory: 233.6 MB (81.50%) - LeetSync

---
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    long long minimumCost(int m, int n, vector<int>& h, vector<int>& v) {
+        sort(h.begin(), h.end(), greater<>());
+        sort(v.begin(), v.end(), greater<>());
+        int vert = 1, horz = 1;
+
+        int row = h.size(), col = v.size();
+        int i=0; 
+        int j=0;
+        long long cost = 0;
+
+        while(i < row and j < col){
+            // horizontal cut
+            if(h[i] > v[j]){
+                cost += h[i] * vert;
+                horz++;
+                i++;
+            }
+            else{ // vertical cut
+                cost += v[j] * horz;
+                vert++;
+                j++;
+            }
+        }
+
+        while(i < row){
+            cost += h[i] * vert;
+            horz++;
+            i++;
+        }
+
+        while(j < col){
+            cost += v[j] * horz;
+            vert++;
+            j++;
+        }
+
+        return cost;
+    }
+};
