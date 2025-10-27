7d18cf439fd8bd8f013d14be0568ede7f475e2a6
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2025-10-27 14:54:54 +0530

    Time: 1 ms (21.31%) | Memory: 8.1 MB (85.87%) - LeetSync

---
+++ 65-valid-number/valid-number.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool seenDigit = false;
+        bool seenDot = false;
+        bool seenExp = false;
+
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+
+            if (isdigit(ch)) {
+                seenDigit = true;
+            } 
+            else if (ch == '+' || ch == '-') {
+                if (i > 0 && !(s[i-1] == 'e' || s[i-1] == 'E'))
+                    return false;
+            } 
+            else if (ch == '.') {
+                
+                if (seenDot || seenExp)
+                    return false;
+                seenDot = true;
+            } 
+            else if (ch == 'e' || ch == 'E') {
+                
+                if (seenExp || !seenDigit)
+                    return false;
+                seenExp = true;
+                seenDigit = false; 
+            } 
+            else {
+                return false;
+            }
+        }
+
+        return seenDigit;
+    }
+};
