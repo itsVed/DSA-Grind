4d6814ee923b5300f7be3c9988862d119538e851
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2025-10-28 10:37:39 +0530

    Time: 3 ms (83.03%) | Memory: 22.1 MB (67.89%) - LeetSync

---
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countValidSelections(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0LL), ls = 0, cases = 0;
+        
+        for (int num : nums) {
+            int rs = total - ls - num;
+
+            if (num == 0) {
+                if (ls == rs){
+                    cases += 2;
+                } else if (abs(ls - rs) == 1) {
+                    cases++;
+                }
+            }
+
+            ls += num;
+        }
+        
+        return cases;
+    }
+};
