824420506991ec1aad4409e5bf274b1fa9f2fe7c
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2025-10-31 14:39:32 +0530

    Time: 2 ms (51.81%) | Memory: 46.4 MB (5.67%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        boolean[] vis = new boolean[n];
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        vis[0] = true;
+
+        while(!q.isEmpty()) {
+            int room = q.poll();
+
+            for(int key : rooms.get(room)) {
+                if(!vis[key]) {
+                    vis[key] = true;
+                    q.add(key);
+                }
+            }
+        }
+
+        for(boolean visited : vis) {
+            if(!visited) return false;
+        }
+        
+        return true;
+    }
+}
