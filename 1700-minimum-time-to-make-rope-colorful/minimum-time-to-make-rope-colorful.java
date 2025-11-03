302cadcfd86cfa4a5d8a29aafd7f611d20511f03
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2025-11-03 09:19:25 +0530

    Time: 7 ms (93.01%) | Memory: 90.7 MB (5.68%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -7,11 +7,9 @@ class Solution {
             if (s.charAt(i) == s.charAt(i - 1)) {
                 if (t[i - 1] < t[i]) {
                     cost += t[i - 1];
-                    t[i - 1] = 0;
                 } else {
                     cost += t[i];
                     t[i] = t[i - 1];
-                    t[i - 1] = 0;
                 }
             }
         }
