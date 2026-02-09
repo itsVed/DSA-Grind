091f416a9f3db7783a7401bd8b3a6d01d04130ea
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2026-02-09 11:08:18 +0530

    Time: 8 ms (54.63%) | Memory: 19.6 MB (29.40%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,55 +1,60 @@
 class Solution {
 public:
 
-    int f(vector<int> &nums) {
-        stack<int> st;
+    int largestRectangleArea(vector<int>& arr) {
         int maxArea = 0;
-        int element, nse, pse;
+        int n = arr.size();
 
-        for(int i = 0; i < nums.size(); i++) {
-            while(!st.empty() && nums[st.top()] > nums[i]) {
-                element = st.top();
+        stack<int> st;
+
+        for(int i=0; i < n; i++){
+            
+
+            while(!st.empty() and arr[st.top()] > arr[i]){
+                int ele = st.top();
                 st.pop();
+                int nse = i;
+                int pse = st.empty() ? -1 : st.top();
 
-                nse = i;
-                pse = st.empty() ? -1 : st.top();
-
-                maxArea = max(maxArea, nums[element] * (nse - pse - 1));
+                maxArea = max(maxArea, (nse - pse - 1) * arr[ele]);
             }
+
             st.push(i);
         }
 
-        while(!st.empty()) {
-            element = st.top();
+        while(!st.empty()){
+            int ele = st.top();
             st.pop();
 
-            nse = nums.size();
-            pse = st.empty() ? -1 : st.top();
+            int nse = n;
+            int pse = st.empty() ? -1 : st.top();
 
-            maxArea = max(maxArea, nums[element] * (nse - pse - 1));
+            maxArea = max(maxArea, (nse - pse - 1) * arr[ele]);
         }
 
         return maxArea;
     }
 
     int maximalRectangle(vector<vector<char>>& matrix) {
+        int maxArea = 0;
         int row = matrix.size();
         int col = matrix[0].size();
 
-        vector<int> vec(col, 0);
-        int ans = 0;
+        vector<int> height(col, 0);
 
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(matrix[i][j] == '1')
-                    vec[j] += 1;
-                else
-                    vec[j] = 0;
+        for(int i=0; i < row; i++){
+            for(int j=0; j < col; j++){
+                if(matrix[i][j] == '1'){
+                    height[j] += 1;
+                }
+                else{
+                    height[j] = 0;
+                }
             }
 
-            ans = max(ans, f(vec));
+            maxArea = max(maxArea, largestRectangleArea(height));
         }
 
-        return ans;
+        return maxArea;
     }
 };
