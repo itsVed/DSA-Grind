a5cb0bd23fe8a7f17eee3664c9c0db25186109e5
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2026-03-08 15:03:05 +0530

    Time: 62 ms (95.23%) | Memory: 31.7 MB (83.37%) - LeetSync

--- 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.py
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.py
@@ -7,7 +7,7 @@ class Solution:
         ans = -1
         
         for i in range(n, -1, -1):
-            if prod > tot:
+            if prod > curr :
                 break
             curr -= nums[i]
             if curr == prod:
