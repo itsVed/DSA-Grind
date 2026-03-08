8d1cd3e33febb343995f12a498d1f2c02a4e4d7a
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2026-03-08 12:18:02 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (99.21%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -1,22 +1,11 @@
 class Solution:
-
-    def solve(self, s, arr, k) :
-
-        if len(s) == k :
-            arr.append(s)
-            return
-
-        self.solve(s + '0', arr, k)
-        self.solve(s + '1', arr, k)
-
-
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        
         arr = []
-        k = len(nums[0])
-        self.solve("", arr, k)
 
-        for i in range(len(arr)) :
+        for i in range(len(nums)) :
+            if nums[i][i] == '0' :
+                arr.append('1')
+            else:
+                arr.append('0')
 
-            if arr[i] not in nums :
-                return arr[i]
+        return ''.join(arr)
