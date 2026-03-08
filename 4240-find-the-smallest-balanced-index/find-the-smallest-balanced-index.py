417d391bce9989a0e1169083c115ac7ee3f68b31
Vedprakash Maliarya <arjunmaliarya@gmail.com>
2026-03-08 15:01:03 +0530

    Time: 64 ms (94.14%) | Memory: 31.6 MB (83.37%) - LeetSync

---
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestBalancedIndex(self, nums: list[int]) -> int:
+        n = len(nums) - 1
+        tot = sum(nums)
+        prod = 1
+        curr = tot
+        ans = -1
+        
+        for i in range(n, -1, -1):
+            if prod > tot:
+                break
+            curr -= nums[i]
+            if curr == prod:
+                ans = i
+            prod *= nums[i]
+
+        return ans
